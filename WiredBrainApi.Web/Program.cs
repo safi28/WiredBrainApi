022683eb82b398c783550604b6a0c9cd8bf2929d
safih28 <safihanadi28@gmail.com>
2025-06-21 13:22:06 +0300

    add azure deploy

--- WiredBrainApi.Web/Program.cs
+++ WiredBrainApi.Web/Program.cs
@@ -2,10 +2,7 @@ using WiredBrainApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -13,17 +10,14 @@ builder.Services.AddTransient<IInventoryService, InventoryService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/", () => "WiredBrain API is running!");
 
 app.Run();
